3c177b8f2e911bf794df389654ba6f201d993cbc
ABDUL_GAFFAR <gk9312@gmail.com>
2021-02-09 17:31:12 +0600

    file added

---
+++ primitivedata.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+//var name string
+//var age int
+//var result bloat32
+//var isFound bool
+
+
+var chr rune
+chr = 'F'
+fmt.Println(chr)
+fmt.Printf("%c", chr)
+
+//fmt.Println(isFound)
+//fmt.Println(true && true)
+//fmt.Println(true && false)
+//fmt.Println(true || true)
+//fmt.Println(true || false)
+//fmt.Println(!true)
+
+}
+
+
