0f292dd1e61d475499363fcf58122011487871cb
ABDUL_GAFFAR <gk9312@gmail.com>
2021-02-09 16:13:14 +0600

    file added

--- variable.go
+++ variable.go
@@ -4,9 +4,18 @@ import "fmt"
 
 func main() {
 
-var name string = "gaffar"
+/*var name string = "gaffar"
 var name2, city string = "abdulgaffar", "pabna"
 
 fmt.Println(name , city, name2 )
+*/
+
+//char
+var c rune = 'A'
+var age int = 10
+var result float32 = 50.95
+
+fmt.Println(c, age, result)
 
 }
+
